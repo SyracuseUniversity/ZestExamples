63e2c831b0de7f9c1d65eadd60d11fd76de70f18
pppizzim <139570664+pppizzim@users.noreply.github.com>
2024-04-05 15:14:05 -0400

    Update ring.sh

--- MPI/ring.sh
+++ MPI/ring.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#SBATCH -e output/ring.err
-#SBATCH -o output/ring.out
-#SBATCH --partition=normal
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --job-name=ring  ##Name of job
+#SBATCH --error=output/%x-%j.err  ##stderr for job with job name and id in the filename
+#SBATCH --output=output/%x-%j.out  ##stdout output for job with job name and id in the filename
 
-mpirun ring
+#SBATCH --partition=normal  ##Slurm partition to use
+#SBATCH --nodes=4  ##Number of nodes to run on
+#SBATCH --ntasks-per-node=1 ##Number of tasks to be launched per node
+#SBATCH --cpus-per-task=1  ##Cores allocated for each task in a job
 
+mpirun ring  ##Launch the MPI application across a set of allocated nodes in the cluster
