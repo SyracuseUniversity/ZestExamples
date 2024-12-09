55712483d4c83a747d7b05c875bb5563a1c09e3d
pppizzim <139570664+pppizzim@users.noreply.github.com>
2024-12-09 10:32:37 -0500

    Update ring.sh
    Added new partitions

--- MPI/ring.sh
+++ MPI/ring.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=output/%x-%j.err  ##stderr for job with job name and id in the filename
 #SBATCH --output=output/%x-%j.out  ##stdout output for job with job name and id in the filename
 
-#SBATCH --partition=normal  ##Slurm partition to use
+#SBATCH --partition=compute_zone2,normal  ##Slurm partitions to use
 #SBATCH --nodes=4  ##Number of nodes to run on
 #SBATCH --ntasks-per-node=1 ##Number of tasks to be launched per node
 #SBATCH --cpus-per-task=1  ##Cores allocated for each task in a job
