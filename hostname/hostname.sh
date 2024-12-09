4dbe0bb58c6107cf088c26ffd6e6e53c04b0e994
pppizzim <139570664+pppizzim@users.noreply.github.com>
2024-12-09 10:33:29 -0500

    Update hostname.sh
    Added new partitions

--- hostname/hostname.sh
+++ hostname/hostname.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -e output/hostname.err
 #SBATCH -o output/hostname.out
-#SBATCH --partition=normal
+#SBATCH --partition=compute_zone2,normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
